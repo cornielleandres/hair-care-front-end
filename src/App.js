eea6b29ef8aa0c91f8686f283115b410aeda3bf4
wisdomtooth <cbtsao47@gmail.com>
2018-12-10 12:37:07 -0600

    add dependencies

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import styled from "styled-components";
+import { Route } from "react-router-dom";
 
 // Components
-import {
-	Header,
-} from './components/index.js';
-
+import { Header } from "./components/index.js";
+import NavBar from "./components/NavBar";
 // Action creators
-import {
-	getSanityCheck,
-} from './store/actions/index.js';
+import { getSanityCheck } from "./store/actions/index.js";
 
 const StyledApp = styled.div`
-	background-color: #c7dbf4;
-	min-height: 100vh;
+  background-color: #c7dbf4;
+  min-height: 100vh;
 
-	.message {
-		text-align: center;
-	}
+  .message {
+    text-align: center;
+  }
 `;
 
 class App extends Component {
-	componentDidMount = () => this.props.getSanityCheck();
+  componentDidMount = () => this.props.getSanityCheck();
 
-	render() {
-		const { message } = this.props;
-		return (
-			<StyledApp>
-				<Header />
-				<p className = 'message'>{ message }</p>
-			</StyledApp>
-		);
-	}
-};
+  render() {
+    const { message } = this.props;
+    return (
+      <StyledApp>
+        <Header />
+        <NavBar />
+        <p className="message">{message}</p>
+      </StyledApp>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
-	message: state.message,
+  message: state.message
 });
 
-export default connect(mapStateToProps, { getSanityCheck })(App);
+export default connect(
+  mapStateToProps,
+  { getSanityCheck }
+)(App);
