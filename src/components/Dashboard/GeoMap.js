8be2fb22c1e3a62df8c6e47322237448cf6d290b
Carlos Vargas <cornielleandres@gmail.com>
2018-12-11 12:19:56 -0500

    Add MapBox geolocation to stylist profiles.

--- src/components/Dashboard/GeoMap.js
+++ src/components/Dashboard/GeoMap.js
@@ -1,8 +1,49 @@
 import React from "react";
+import axios from 'axios';
+import mapboxgl from "mapbox-gl";
+import styled from 'styled-components';
 
-class Map extends Component {
-  render() {
-    <div>Map</div>;
+const StyledGeoMap = styled.div`
+  margin: 10px;
+
+  #map {
+    width: 300px;
+    height: 300px;
   }
-}
-export default Map;
+`;
+
+export default class GeoMap extends React.Component {
+  componentDidMount() {
+    const { address, city, state, zip } =  this.props;
+    const geoAddress = `${address} ${city} ${state} ${zip}`;
+    const mapBoxAccessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+    axios
+      .get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${ geoAddress }.json?access_token=${ mapBoxAccessToken }`)
+      .then(res => {
+        const center = res.data.features[0].center;
+
+        mapboxgl.accessToken = mapBoxAccessToken;
+        const map = new mapboxgl.Map({
+          container: 'map',
+          center,
+          zoom: 14,
+          style: 'mapbox://styles/mapbox/streets-v10',
+          hash: true,
+        });
+
+        new mapboxgl.Marker()
+          .setLngLat(center)
+          .addTo(map);
+      })
+      .catch(err => console.log(err.response));
+	};
+
+  render() {
+    return(
+      <StyledGeoMap>
+        <div id = 'map' />
+      </StyledGeoMap>
+    );
+  }
+};
--- src/components/Dashboard/ProfileCard.js
+++ src/components/Dashboard/ProfileCard.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import GeoMap from './GeoMap.js';
 
 const ProfileCard = ({ stylist }) => {
+  const { address, city, state, zip } = stylist;
   return (
     <div>
       <h4>picture</h4>
@@ -11,6 +13,7 @@ const ProfileCard = ({ stylist }) => {
       <h4>average cost</h4>
       <textarea>description</textarea>
       <h4>links to social media</h4>
+      <GeoMap address = { address } city = { city } state = { state } zip = { zip } />
     </div>
   );
 };
