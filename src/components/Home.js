9fc6dc7fe790f68bf825f3287da8fc8f16888baa
Carlos Vargas <cornielleandres@gmail.com>
2018-12-12 21:20:28 -0500

    List images in home component.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+import Image from './Image';
+import axios from 'axios';
 
-const Home = props => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+export default class Home extends Component {
+  state = { pictures: [] };
+  componentDidMount() {
+    const token = localStorage.getItem("userToken");
+    const headers = { headers: { Authorization: `${token}` } };
+    axios
+      .get(`${process.env.REACT_APP_BACKEND_URL}/api/pictures`, headers)
+      .then(res => {
+        this.setState({ pictures: res.data });
+      })
+      .catch(err => console.log(err.response));
+  }
+  render() {
+    const { pictures } = this.state;
+    return (
+      <div>
+        <h1>Home</h1>
+        { pictures.map((picture, i) => <Image key = {i} picture = {picture} />)}
+      </div>
+    );
+  }
 };
-
-export default Home;
---
+++ src/components/Image.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Image = ({picture}) => {
+	return(
+		<div>
+			<img src={picture.portfolio_picture || 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'} />
+			<p>Created at: {picture.created_at}</p>
+		</div>
+	);
+};
+
+export default Image;
