7da2c438d33b7b88ff2ec663c9b603cece28aaf7
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2018-12-13 22:28:43 -0500

    Style ProfileCard and MyProfile for consistency

--- src/components/Dashboard/Profiles/ProfileCard.js
+++ src/components/Dashboard/Profiles/ProfileCard.js
@@ -3,56 +3,57 @@ import styled from "styled-components";
 
 const StyledProfileCard = styled.div`
 display: flex;
-justify-content: space-evenly;
-align-items: center;
 flex-wrap: wrap;
 flex-direction: column;
-border: 5px solid pink;
+justify-content: center;
+align-items: flex-start;
+border: 5px solid #f9899e;
 border-radius: 15px;
 margin: 10px 15px;
-padding: 10px 3%;
+padding: 20px;
 width: 325px;
 background: #1d0b32;
 
-.stylist-card-img {
-  margin: 15px 0;
 
   img {
     border-radius: 15px;
     max-width: 300px;
     max-height: 300px;
+    margin: 0 auto;
+    margin-bottom: 15px;
   }
-}
+
 
 .stylist-info {
   display: flex;
   align-items: flex-start;
   justify-content: flex-start;
+  padding: 0 15px;
+  margin-bottom: 15px;
 
   span {
     font-size: 1.8rem;
     font-weight: 900;
-    color: pink;
+    color: #f9899e;
   }
+
+  h4 {
+    font-size: 1.8rem;
+      color: #f9899e;
+      font-family: 'Muli';
+      margin-bottom: 10px;
+      
+    }
 }
 
-h4 {
-font-size: 1.8rem;
-  color: pink;
-  font-family: 'Muli';
-  margin-bottom: 10px;
-  
-}
+
 `;
 
 const ProfileCard = props => {
   const { stylist } = props;
   return (
     <StyledProfileCard>
-      <div className="stylist-card-img">
         <img src={stylist.profile_photo} alt="picture" />
-      </div>
-
       <div className="stylist-info">
         <span>Name:&nbsp;</span>
         <h4>{stylist.first_name}</h4>
