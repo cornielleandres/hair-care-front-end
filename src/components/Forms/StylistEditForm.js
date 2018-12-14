26d64bd0be79b3f5b7bbe683da31cabe9a5fbc28
wisdomtooth <cbtsao47@gmail.com>
2018-12-13 21:34:16 -0600

    add edit form style

--- src/components/Forms/StylistEditForm.js
+++ src/components/Forms/StylistEditForm.js
@@ -1,5 +1,52 @@
 import React, { Component } from "react";
 import Axios from "axios";
+import styled from "styled-components";
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 100px;
+
+  input {
+    padding: 15px;
+    border: 3px solid #f9899e;
+    border-radius: 12px;
+    margin-bottom: 25px;
+    width: 100%;
+    font-size: 1.8rem;
+    font-family: "Muli";
+
+    :focus {
+      outline: none;
+      font-size: 1.8rem;
+      color: #4947e5;
+      font-weight: 800;
+    }
+
+    ::placeholder {
+      font-size: 1.8rem;
+    }
+  }
+
+  button {
+    border: 2px solid #f9899e;
+    border-radius: 15px;
+    background: #1d0b32;
+    color: #f9899e;
+    font-size: 1.8rem;
+    font-family: "Muli";
+    padding: 10px 20px;
+    margin-top: 20px;
+    margin-bottom: 15px;
+    cursor: pointer;
+
+    :hover {
+      transform: scale(1.1, 1.1);
+      background-color: #4947e5;
+    }
+  }
+`;
 
 class StylistSignUpForm extends Component {
   constructor() {
@@ -40,62 +87,60 @@ class StylistSignUpForm extends Component {
   render() {
     console.log(this);
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            name="profile_photo"
-            value={this.state.profile_photo}
-            placeholder="picture of yourself"
-            onChange={this.handleChange}
-          />
-          <input
-            name="first_name"
-            value={this.state.username}
-            type="text"
-            placeholder="First Name"
-            onChange={this.handleChange}
-          />
-          <input
-            name="last_name"
-            value={this.state.last_name}
-            type="text"
-            placeholder="Last Name"
-            onChange={this.handleChange}
-          />
-          <input
-            name="address"
-            value={this.state.address}
-            type="text"
-            placeholder="Address"
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            name="city"
-            value={this.state.city}
-            placeholder="City"
-            onChange={this.handleChange}
-          />
-          <input
-            type="text"
-            name="state"
-            value={this.state.state}
-            type="text"
-            placeholder="State"
-            onChange={this.handleChange}
-          />
-          <input
-            name="zip"
-            value={this.state.zip}
-            type="text"
-            placeholder="Zip"
-            onChange={this.handleChange}
-          />
+      <StyledForm onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          name="profile_photo"
+          value={this.state.profile_photo}
+          placeholder="picture of yourself"
+          onChange={this.handleChange}
+        />
+        <input
+          name="first_name"
+          value={this.state.username}
+          type="text"
+          placeholder="First Name"
+          onChange={this.handleChange}
+        />
+        <input
+          name="last_name"
+          value={this.state.last_name}
+          type="text"
+          placeholder="Last Name"
+          onChange={this.handleChange}
+        />
+        <input
+          name="address"
+          value={this.state.address}
+          type="text"
+          placeholder="Address"
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="city"
+          value={this.state.city}
+          placeholder="City"
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="state"
+          value={this.state.state}
+          type="text"
+          placeholder="State"
+          onChange={this.handleChange}
+        />
+        <input
+          name="zip"
+          value={this.state.zip}
+          type="text"
+          placeholder="Zip"
+          onChange={this.handleChange}
+        />
 
-          <button type="submit">Edit Information</button>
-        </form>
-      </div>
+        <button type="submit">Edit Information</button>
+      </StyledForm>
     );
   }
 }
